c63c2ca7b981b8c9b5884633cbf22befb5fdd714
黄新民 <huangxinmin@hzwanglan.com>
2022-08-15 08:39:07 +0800

    220815 增加swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Get } from '@nestjs/common'
+import { ApiOperation, ApiTags } from '@nestjs/swagger'
 import { AppService } from './app.service'
 
 @Controller()
+@ApiTags('App 总模块')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({
+    summary: '测试接口'
+  })
   getHello(): string {
     return this.appService.getGoodbye()
   }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 const listenPort = 3000
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  const options = new DocumentBuilder()
+    .setTitle('项目管理平台')
+    .setDescription('xxx项目管理平台')
+    .setVersion('1.0')
+    // .addTag('cats')
+    .build()
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('api', app, document)
   console.log(`listen in http://localhost:${listenPort}`)
   await app.listen(listenPort)
 }
