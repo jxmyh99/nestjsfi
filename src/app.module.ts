5fcc33c698399985a24875cebb0c2a875e013766
黄新民 <huangxinmin@hzwanglan.com>
2022-08-17 20:50:47 +0800

    完成log4js的配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common'
+import { Log4jsModule } from '@nestx-log4js/core'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { DbModule } from './db/db.module'
 import { UserModule } from './modules/user/user.module'
 
 @Module({
-  imports: [DbModule, UserModule],
+  imports: [DbModule, UserModule, Log4jsModule.forRoot()],
   controllers: [AppController],
   providers: [AppService]
 })
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,35 @@
+import { Logger } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { Log4jsLogger } from '@nestx-log4js/core'
 import { AppModule } from './app.module'
 
 const listenPort = 3000
-
-async function bootstrap() {
+// 原生日志
+const logger = new Logger('main.ts')
+/**
+ * 启动主方法
+ */
+const bootstrap = async () => {
   const app = await NestFactory.create(AppModule)
+  /**
+   * 配置 swgger
+   */
   const options = new DocumentBuilder()
     .setTitle('项目管理平台')
-    .setDescription('xxx项目管理平台')
+    .setDescription('xxx接口文档')
     .setVersion('1.0')
     // .addTag('cats')
     .build()
   const document = SwaggerModule.createDocument(app, options)
-  SwaggerModule.setup('api', app, document)
-  console.log(`listen in http://localhost:${listenPort}`)
+  SwaggerModule.setup('swagger-ui', app, document)
+  /**
+   * 使用log4js 日志框架
+   */
+  app.useLogger(app.get(Log4jsLogger))
   await app.listen(listenPort)
 }
-bootstrap()
+
+bootstrap().then(() => {
+  logger.log(`listen in http://localhost:${listenPort}`)
+})
